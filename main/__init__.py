b96fc49b07c640889b4374eaec35b670e2f0baa2
sᴀɢᴀʀ ᴛɪᴡᴀʀɪ <112709751+stkeditz@users.noreply.github.com>
2024-10-01 08:08:59 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -17,7 +17,7 @@ API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
 SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
+#FORCESUB = config("FORCESUB", default=None)
 AUTH = config("AUTH", default=None)
 SUDO_USERS = []
 
